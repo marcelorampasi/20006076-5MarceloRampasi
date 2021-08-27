c786b8f29bf6bbf3c1e54137ff6fa66c41e89e6d
marcelorampasi <88569317+marcelorampasi@users.noreply.github.com>
2021-08-26 21:34:27 -0300

    Add files via upload

--- visualizaproduto.php
+++ visualizaproduto.php
@@ -1,13 +1,21 @@
 <?php 
 	include "topo.php";
 	include "dados/arrayprodutos.php";
-    $prod = $_GET["produto"];
-    $key = array_search($prod, array_column($arrayproduto, $_GET["produto"]));
+    $prod = $_GET['produto'];
+    $nomeprod = array_search($prod, array_column($arrayproduto, $valor[1]));
+	$fotoprod = array_search($prod, array_column($arrayproduto, $valor[4]));
 ?>
 
 			<div class="titulo"> DETALHAMENTO DE PRODUTO </br></br></div>
 			<div class="produto">
-                     <?php echo $key ?>
+					<table>
+							<tr>
+								<td class="imgproduto"><img src="images/<?php echo $$valor[4] ?>"/><?php echo $$valor[4] ?></td>
+							</tr>
+							<tr>
+							<td><?php echo $$valor[1] ?></br> R$ <?php echo $$valor[3] ?>,00</td>
+							</tr>
+						</table>
 				
 			</div>
 <?php include "rodape.php" ?>
